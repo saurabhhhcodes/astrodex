27404b078e4a5d8865a372b391d45aae4e1af312
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-31 13:17:36 +0530

    fix: add safety checks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,46 +11,4 @@ import { Toasts } from "@/components/Toasts"
 import { KeyboardNavigation } from "@/components/KeyboardNavigation"
 
 const Scene = dynamic(() => import("@/components/Scene").then((m) => ({ default: m.Scene })), {
-  ssr: false,
-})
-
-import { useEffect } from "react"
-import { useAppState } from "@/lib/store"
-
-export default function Home({ initialObjectId }: { initialObjectId?: string | null }) {
-  const { setFocusedObjectId } = useAppState()
-
-  useEffect(() => {
-    if (initialObjectId) {
-      setFocusedObjectId(initialObjectId)
-    }
-  }, [initialObjectId, setFocusedObjectId])
-
-  return (
-    <main
-      style={{
-        position: "relative",
-        width: "100%",
-        height: "100%",
-        overflow: "hidden",
-        background: "#000005",
-      }}
-    >
-      {/* Background 3D Space Scene */}
-      <Scene />
-
-      {/* HUD UI Layout Components */}
-      <Header />
-      <LeftSidebar />
-      <RightSidebar />
-      <AgentTerminal />
-      <Toasts />
-      
-      {/* Floating Asteroid Inspector */}
-      <AsteroidCard />
-      
-      {/* Global Keyboard Navigation */}
-      <KeyboardNavigation />
-    </main>
-  )
-}
+.catch(err=>console.error(err))
